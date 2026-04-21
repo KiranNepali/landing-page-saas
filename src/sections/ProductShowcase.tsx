00baea2971a45056399793f155d33d63c3c52602
KiranNepali <itsmekiran44@gmail.com>
2026-04-21 23:54:49 +0300

    add animation on product showcase section

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,30 +1,61 @@
+"use client"
+
 import productImage from "@/assets/product-image.png";
 import pyramidImage from "@/assets/pyramid.png";
-import  tubeImage from "@/assets/tube.png";
+import tubeImage from "@/assets/tube.png";
 import Image from "next/image";
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from "react";
+import { section } from "framer-motion/client";
 export const ProductShowcase = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ["start end", "end start"],
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150]);
   return (
-    <section className="bg-gradient-to-b from-[#FFFFFF] to to-[#D2DCFF] py-[24] overflow-x-clip">
+    <section
+      ref={sectionRef}
+      className="bg-gradient-to-b from-[#FFFFFF] to to-[#D2DCFF] py-[24] overflow-x-clip"
+    >
       <div className="container">
-       <div className="section-heading">
-         <div className="flex justify-center">
-          <div className="tag">Boost your productivity</div>
+        <div className="section-heading">
+          <div className="flex justify-center">
+            <div className="tag">Boost your productivity</div>
+          </div>
+          <h2 className="section-title mt-5">
+            A more effective way to track progress
+          </h2>
+          <p className="section-description mt-5">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
+            laboriosam dolorem necessitatibus perspiciatis fuga repellendus
+            recusandae reprehenderit dolore facilis incidunt.
+          </p>
         </div>
-        <h2 className="section-title mt-5">
-          A more effective way to track progress
-        </h2>
-        <p className="section-description mt-5">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet,
-          laboriosam dolorem necessitatibus perspiciatis fuga repellendus
-          recusandae reprehenderit dolore facilis incidunt.
-        </p>
-       </div>
-       <div className="relative">
-
-        <Image src={productImage} alt="Product Image" className="mt-10" />
-        <Image src={pyramidImage} alt="Pyramid Image" height={262} width={262} className="hidden md:block absolute -right-36 -top-32" />
-        <Image src={tubeImage} alt="Tube Image" height={248} className="hidden md:block absolute bottom-24 -left-36" />
-       </div>
+        <div className="relative">
+          <Image src={productImage} alt="Product Image" className="mt-10" />
+          <motion.img
+            src={pyramidImage.src}
+            alt="Pyramid Image"
+            height={262}
+            width={262}
+            className="hidden md:block absolute -right-36 -top-32"
+            style={{
+              translateY: translateY
+            }}
+          />
+          <motion.img
+            src={tubeImage.src}
+            alt="Tube Image"
+            height={248}
+            width={248}
+            className="hidden md:block absolute bottom-24 -left-36"
+            style={{
+              translateY: translateY
+            }}
+          />
+        </div>
       </div>
     </section>
   );
