25b766d0233cb3852cad193db2e77bbb702159aa
KiranNepali <itsmekiran44@gmail.com>
2026-04-21 23:06:52 +0300

    crate call to action section

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,30 @@
+import ArrowIcon from "@/assets/arrow-right.svg";
+import starImage from "@/assets/star.png";
+import springImage from "@/assets/spring.png";
+import Image from "next/image";
+
 export const CallToAction = () => {
-  return null;
+  return (
+    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
+      <div className="container">
+        <div className="section-heading relative">
+          <h2 className="section-title">Sign up for free today</h2>
+          <p className="section-description mt-5">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit,
+            distinctio! Numquam rerum eligendi delectus adipisci veritatis
+            aperiam cupiditate nostrum recusandae.
+          </p>
+          <Image src={starImage} alt="Star Image" width={360} className="absolute -left-[350px] -top-[137px]"/>
+           <Image src={springImage} alt="Star Image" width={360} className="absolute -right-[331px] -top-[19px]"/>
+        </div>
+        <div className="flex gap-2 mt-10 justify-center">
+          <button className="btn btn-primary">Get for free</button>
+          <button className="btn btn-text gap-1">
+            <span>Learn more</span>
+            <ArrowIcon className="w-5 h-5" />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
